fc4929e4f7efbff77714bdd59d0601ffc7d0c812
momonchy <amadeus475@gmail.com>
2024-10-05 00:09:36 +0900

    第1回を追加

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf(" /\\___/\\\t_\t _\n/\t\\  |_| ");
+    printf("|_ |  |  | |  /\n\\ * _ * /  |");
+    printf(" | |_ |_ |_ |_| *\n \\_____/\n");
+    return 0;
+}
